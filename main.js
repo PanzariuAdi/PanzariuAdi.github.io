673a1800e730685c9ac29c4d73fb1842fd7037ec
PanzariuAdi <panzariu.adi00@gmail.com>
2023-05-16 16:32:51 -0400

    Improved appearence

--- main.js
+++ main.js
@@ -1,60 +1,34 @@
 const inputElement = document.getElementById('commandInput');
-const lineCommand = document.getElementById('lineCommand');
 const terminal = document.getElementById('response');
 const userElement = document.getElementById('user');
 const pathElement = document.getElementById('path');
 const branchElement = document.getElementById('branch');
+const ENTER_KEY = 13;
+const ASCII_NAME = `
+    ██████╗░░█████╗░███╗░░██╗███████╗░█████╗░██████╗░██╗██╗░░░██╗  ░█████╗░██████╗░██╗
+    ██╔══██╗██╔══██╗████╗░██║╚════██║██╔══██╗██╔══██╗██║██║░░░██║  ██╔══██╗██╔══██╗██║
+    ██████╔╝███████║██╔██╗██║░░███╔═╝███████║██████╔╝██║██║░░░██║  ███████║██║░░██║██║
+    ██╔═══╝░██╔══██║██║╚████║██╔══╝░░██╔══██║██╔══██╗██║██║░░░██║  ██╔══██║██║░░██║██║
+    ██║░░░░░██║░░██║██║░╚███║███████╗██║░░██║██║░░██║██║╚██████╔╝  ██║░░██║██████╔╝██║
+    ╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░╚═════╝░  ╚═╝░░╚═╝╚═════╝░╚═╝
+`;
 
 let user = userElement.innerText;
 let path = pathElement.innerText;
 let branch = branchElement.innerText;
 
-const ENTER_KEY = 13;
+let validCommands = ['help', 'clear', 'pwd', 'cd', 'ls', 'about', 'cv', 'init'];
 
-let validCommands = ['help', 'clear', 'pwd', 'about', 'cv'];
-
-function showInitialMessage() {
-    let result = document.createElement("pre");
-    result.style.height = "150px";
-
-    result.innerHTML += `
-        
-        ██████╗░░█████╗░███╗░░██╗███████╗░█████╗░██████╗░██╗██╗░░░██╗  ░█████╗░██████╗░██╗
-        ██╔══██╗██╔══██╗████╗░██║╚════██║██╔══██╗██╔══██╗██║██║░░░██║  ██╔══██╗██╔══██╗██║
-        ██████╔╝███████║██╔██╗██║░░███╔═╝███████║██████╔╝██║██║░░░██║  ███████║██║░░██║██║
-        ██╔═══╝░██╔══██║██║╚████║██╔══╝░░██╔══██║██╔══██╗██║██║░░░██║  ██╔══██║██║░░██║██║
-        ██║░░░░░██║░░██║██║░╚███║███████╗██║░░██║██║░░██║██║╚██████╔╝  ██║░░██║██████╔╝██║
-        ╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░╚═════╝░  ╚═╝░░╚═╝╚═════╝░╚═╝
-    `;
-    result.innerHTML += "<br>"
-    result.innerHTML += "\tType help and hit enter to see all the commands."
-    terminal.appendChild(result);
-
-}
-
-showInitialMessage();
 
 inputElement.addEventListener("input", isValidCommand);
+inputElement.addEventListener('keypress', handleKeyPress);
 
-function isValidCommand() {
-    if (validCommands.includes(inputElement.value)) {
-        inputElement.style.color = "green";
-    } else {
-        inputElement.style.color = "red";
+
+function handleKeyPress(event) {
+    if (event.keyCode === ENTER_KEY) {
+        const command = inputElement.value.trim();
+        executeCommand(command);
     }
-}
-
-function addInfoLine(command) {
-    let result = document.createElement("div");
-    let info = document.createElement("p");
-
-    info.innerHTML += user + " :: ";
-    info.innerHTML += path + " |";
-    info.innerHTML += branch + "|";
-    info.innerHTML += ` ^ ${command}`;
-    result.appendChild(info);
-    terminal.appendChild(result);
-    
 }
 
 function executeCommand(command) {
@@ -77,10 +51,69 @@ function executeCommand(command) {
         case 'cv':
             cv();
             break;
+        case 'cd':
+            cd();
+            break;
+        case 'ls':
+            ls();
+            break;
+        case 'init':
+            showInitialMessage();
+            break;
     }
 
-    window.scrollTo(0, document.body.scrollHeight);
+    terminal.scrollIntoView(false);
+    window.scrollTo(0, terminal.height);
 }
+
+function addInfoLine(command) {
+    let result = document.createElement("div");
+    result.setAttribute("class", "command");
+
+    let localUser = document.createElement("p");
+    localUser.setAttribute("class", "infoLine user");
+    localUser.innerHTML += user;
+    result.appendChild(localUser);
+
+
+    let localPath = document.createElement("p");
+    localPath.setAttribute("class", "infoLine path");
+    localPath.innerHTML += path;
+    result.appendChild(localPath);
+
+    let localBranch = document.createElement("p");
+    localBranch.setAttribute("class", "infoLine branch");
+    localBranch.innerHTML += branch;
+    result.appendChild(localBranch);
+
+    let cmd = document.createElement("p");
+    cmd.setAttribute("class", "infoLine");
+    cmd.innerHTML += command;
+    result.appendChild(cmd);
+
+    terminal.appendChild(result);
+}
+
+function showInitialMessage() {
+    let result = document.createElement("pre");
+    result.style.height = "25%";
+
+    result.innerHTML += ASCII_NAME;
+    result.innerHTML += "<br>"
+    result.innerHTML += "    Type help and hit enter to see all the commands."
+    terminal.appendChild(result);
+}
+
+showInitialMessage();
+
+function isValidCommand() {
+    if (validCommands.includes(inputElement.value)) {
+        inputElement.style.color = "green";
+    } else {
+        inputElement.style.color = "red";
+    }
+}
+
 
 function clear() {
     terminal.innerHTML = '';
@@ -123,11 +156,14 @@ function cv() {
     terminal.appendChild(result);
 }
 
-function handleKeyPress(event) {
-    if (event.keyCode === ENTER_KEY) {
-        const command = inputElement.value.trim();
-        executeCommand(command);
-    }
+function cd() {
+    path += "/folder";
+    document.getElementById("path").innerText = path;
 }
 
-inputElement.addEventListener('keypress', handleKeyPress);
+function ls() {
+    let result = document.createElement("div");
+    result.innerHTML += "<p>TO BE IMPLEMENTED. DO NOT KNOW WHAT TO DO WITH THIS YET.</p>";
+    terminal.appendChild(result);
+}
+
